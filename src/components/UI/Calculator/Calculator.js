e9c58f96c074a99256d098a20c82b0e58abc987c
Harkirat Gill <harkiratgill@Harkirats-MacBook-Pro.local>
2019-11-11 22:45:41 -0800

    working input

--- src/components/UI/Calculator/Calculator.js
+++ src/components/UI/Calculator/Calculator.js
@@ -13,13 +13,13 @@ export default class Calculator extends React.Component {
     }
 
     handleInterest(event){
-        this.setState({interest: event})
+        this.setState({interest: event.target.value})
     }
     handlePrincipal(event) {
-        this.setState({ principal: event })
+        this.setState({ principal: event.target.value })
     }
     handlePeriod(event) {
-        this.setState({ period: event })
+        this.setState({ period: event.target.value })
     }
 
     render(){
@@ -29,19 +29,19 @@ export default class Calculator extends React.Component {
                 <header className="App-header">
                     <Input 
                         label="Mortgage ($)" 
-                        onChange={() => this.handlePrincipal()} 
+                        onChange={this.handlePrincipal.bind(this)} 
                         type="text" 
                         name="name" 
                         placeholder="Your interest %" />
                     <Input 
                         label="Period (yrs)"
-                        onChange={() => this.handlePeriod()} 
+                        onChange={this.handlePeriod.bind(this)} 
                         type="email" 
                         name="name" 
                         placeholder="Your interest %" />
                     <Input 
                         label="Interest (%)"
-                        onChange={() => this.handleInterest()} 
+                        onChange={this.handleInterest.bind(this)} 
                         type="text" 
                         name="name" 
                         placeholder="Your interest %" />
